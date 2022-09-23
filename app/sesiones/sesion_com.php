cd5b7e1090a8f77ea7454765bd5d04e9b232ffdf
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2022-09-23 16:30:54 -0500

    fix login

--- app/sesiones/sesion_com.php
+++ app/sesiones/sesion_com.php
@@ -2,15 +2,28 @@
 @session_start();
 
 if (empty($_SESSION['active'])) {
-  header("location: ../../index.php");
-} else if ($_SESSION['rol'] > 2) {
+  header("location: /index.php");
+}
+
+if ($_SESSION['rol'] > 2) {
   session_destroy();
-  header("location: ../../index.php");
-} else if (isset($_SESSION["timeout"])) {
+  header("location: /index.php");
+}
+
+if (isset($_SESSION["timeout"])) {
   $inactividad = 1500;
   $sessionTTL = time() - $_SESSION["timeout"];
+
   if ($sessionTTL > $inactividad) {
     session_destroy();
-    header("location: ../../index.php");
+    header("location: /index.php");
   }
 }
+
+/* @session_start();
+if (empty($_SESSION['active']) || time() - $_SESSION['time'] > 5600) {
+  session_destroy();
+  echo "<script> window.location='/index.php'; </script>";
+  exit();
+} else
+  @session_start(); */
