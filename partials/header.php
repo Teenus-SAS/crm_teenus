4296c4d0b3bae8326272e74b682b8a483695540c
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2022-10-11 07:05:54 -0500

    fix header session avatar

--- partials/header.php
+++ partials/header.php
@@ -108,7 +108,7 @@
 		</div>
 		<div class="user-box dropdown">
 			<a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-				<img src="<?php echo $_SESSION['avatar'] ?>" class="user-img" alt="user avatar">
+				<img src="<?php if (!empty($_SESSION['avatar'])) echo $_SESSION['avatar'] ?>" class="user-img" alt="user avatar">
 				<div class="user-info ps-3">
 					<p class="user-name mb-0"><?php echo $_SESSION['name'] . ' ' . $_SESSION['lastname'];  ?></p>
 					<p class="designattion mb-0"><?php echo $_SESSION['position'] ?></p>
