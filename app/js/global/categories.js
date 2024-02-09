400c6ff791ad923b5ffac348d2a729e1a24c0dc0
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2024-02-09 14:06:17 -0500

    change select categories

--- app/js/global/categories.js
+++ app/js/global/categories.js
@@ -1,13 +1,33 @@
 /* Load companies */
 
-$(document).ready(function() {
+$(document).ready(function () {
+  $.ajax({
+    url: "/api/categories",
+    success: function (r) {
+      sessionStorage.setItem("categories", JSON.stringify(r));
 
-    $.ajax({
-        url: '/api/categories',
-        success: function(r) {
-            sessionStorage.setItem('categories', JSON.stringify(r))
+      // Crear un objeto para almacenar las categorías únicas
+      var uniqueCategories = {};
 
-            let $select = $(`#selectCategory`)
+      // Filtrar y almacenar las categorías únicas en el objeto
+      r.forEach(function (item) {
+        uniqueCategories[item.id_category] = item.category;
+      });
+
+      // Limpiar el select
+      var $select = $("#selectCategory").empty();
+
+      // Agregar la opción predeterminada al select
+      $select.append("<option disabled selected>Categorias</option>");
+
+      // Iterar sobre las categorías únicas y agregarlas al select
+      for (var categoryId in uniqueCategories) {
+        $select.append(
+          `<option value="${categoryId}">${uniqueCategories[categoryId]}</option>`
+        );
+      }
+
+      /* let $select = $(`#selectCategory`)
             $select.empty()
 
             $select.append(`<option option disabled selected > Categorias</option > `)
@@ -22,28 +42,28 @@ $(document).ready(function() {
                         `<option option value = ${r[i].id_category}> ${r[i].category} </option > `,
                     )
                 }
-            }
-        },
-    })
-})
+            } */
+    },
+  });
+});
 
 /* Cargar subcategorias */
 
-$('#selectCategory').change(function(e) {
-    e.preventDefault();
+$("#selectCategory").change(function (e) {
+  e.preventDefault();
 
-    $select = $(`#selectSubcategory`)
-    $select.empty()
+  $select = $(`#selectSubcategory`);
+  $select.empty();
 
-    categories = sessionStorage.getItem('categories')
-    r = JSON.parse(categories)
-    category = $('#selectCategory').val()
+  categories = sessionStorage.getItem("categories");
+  r = JSON.parse(categories);
+  category = $("#selectCategory").val();
 
-    for (let i = 0; i < r.length; i++) {
-        if (category == r[i].id_category) {
-            $select.append(
-                `<option option value = ${r[i].id_subcategory}> ${r[i].subcategory} </option > `,
-            )
-        }
+  for (let i = 0; i < r.length; i++) {
+    if (category == r[i].id_category) {
+      $select.append(
+        `<option option value = ${r[i].id_subcategory}> ${r[i].subcategory} </option > `
+      );
     }
+  }
 });
